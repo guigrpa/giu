8df8a8496372105c6e19be2571754b62373b85af
G. Grau <ggrau@me.com>
2017-05-08 15:20:42 +0200

    [examples] Apply prettier

--- packages/giu-examples/src/demo1-common.js
+++ packages/giu-examples/src/demo1-common.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-console, no-alert, max-len */
 /* eslint-disable react/prop-types, react/no-multi-comp, react/jsx-no-bind, react/jsx-boolean-value */
 /* eslint-disable react/prefer-stateless-function */
-import React                from 'react';
-import faker                from 'faker';
-import { LIST_SEPARATOR }   from 'giu';
+import React from 'react';
+import faker from 'faker';
+import { LIST_SEPARATOR } from 'giu';
 
-const ExampleLabel = ({ children }) => <div style={style.label}>{children}</div>;
+const ExampleLabel = ({ children }) => (
+  <div style={style.label}>{children}</div>
+);
 
 const style = {
   label: {
@@ -25,13 +27,24 @@ const exampleStyle = {
 
 let lang = 'en';
 const getLang = () => lang;
-const setLang = newLang => { lang = newLang; };
+const setLang = newLang => {
+  lang = newLang;
+};
 
 const NORMAL_OPTIONS = [
-  { label: () => (lang === 'es' ? 'Manzanas' : 'Apples'), value: 'a', keys: 'mod+a', onClick: () => console.log('Custom click Apples') },
+  {
+    label: () => (lang === 'es' ? 'Manzanas' : 'Apples'),
+    value: 'a',
+    keys: 'mod+a',
+    onClick: () => console.log('Custom click Apples'),
+  },
   { label: '2', value: 2, keys: 'mod+2' },
   LIST_SEPARATOR,
-  { label: () => (lang === 'es' ? 'Bayas' : 'Berries'), value: 'b', keys: ['shift+b', 'shift+c'] },
+  {
+    label: () => (lang === 'es' ? 'Bayas' : 'Berries'),
+    value: 'b',
+    keys: ['shift+b', 'shift+c'],
+  },
   { label: 'true', value: true, keys: 'alt+backspace' },
   { label: 'C', value: 'c', keys: 'shift+up' },
 ];
@@ -45,7 +58,10 @@ for (let i = 0; i < 50; i++) {
 }
 
 const WIDE_OPTIONS = [
-  { label: 'A long, really long, very very very very very very long option', value: 'a' },
+  {
+    label: 'A long, really long, very very very very very very long option',
+    value: 'a',
+  },
   { label: 'Another long, really long, very very long option', value: 2 },
   LIST_SEPARATOR,
   { label: 'B', value: 'b' },
@@ -59,9 +75,14 @@ const onChange = (ev, o) => console.log(o);
 const onChangeJson = (ev, o) => console.log(JSON.stringify(o));
 
 export {
-  ExampleLabel, exampleStyle,
-  NORMAL_OPTIONS, TALL_OPTIONS, WIDE_OPTIONS,
+  ExampleLabel,
+  exampleStyle,
+  NORMAL_OPTIONS,
+  TALL_OPTIONS,
+  WIDE_OPTIONS,
   LONG_TEXT,
-  onChange, onChangeJson,
-  getLang, setLang,
+  onChange,
+  onChangeJson,
+  getLang,
+  setLang,
 };
