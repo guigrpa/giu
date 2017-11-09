28911411c4ef189239a552c32a3df550d0fdcab2
Guillermo Grau <guillermo.grau@isardsat.cat>
2017-11-09 16:46:24 +0100

    Revert docs changes

--- packages/giu-examples/next.config.js
+++ packages/giu-examples/next.config.js
@@ -2,7 +2,10 @@ module.exports = {
   exportPathMap: () => ({
     '/': { page: '/' },
     '/demo1': { page: '/demo1' },
+    '/demo1.html': { page: '/demo1' },
     '/demo2': { page: '/demo2' },
+    '/demo2.html': { page: '/demo2' },
     '/demo3': { page: '/demo3' },
+    '/demo3.html': { page: '/demo3' },
   }),
 };
