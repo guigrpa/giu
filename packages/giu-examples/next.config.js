7d85e46f60b9b9f52d18836c055f378a6052b3f7
Guillermo Grau <guillermo.grau@isardsat.cat>
2017-11-10 08:49:13 +0100

    Deploy docs (WIP)

--- packages/giu-examples/next.config.js
+++ packages/giu-examples/next.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   exportPathMap: () => ({
     '/': { page: '/' },
-    '/demo1': { page: '/demo1' },
-    '/demo1.html': { page: '/demo1' },
-    '/demo2': { page: '/demo2' },
-    '/demo2.html': { page: '/demo2' },
-    '/demo3': { page: '/demo3' },
-    '/demo3.html': { page: '/demo3' },
+    '/demos/compact': { page: '/demo1' },
+    '/demos/material': { page: '/demo2' },
+    '/demos/datatable': { page: '/demo3' },
   }),
 };
