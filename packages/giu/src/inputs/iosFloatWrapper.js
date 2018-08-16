e48a97302b0d2c706cc26e928c48bb7081891543
G. Grau <guigrpa@gmail.com>
2018-08-16 13:55:14 +0200

    Improve drop-down menus and list pickers in iOS

--- packages/giu/src/inputs/iosFloatWrapper.js
+++ packages/giu/src/inputs/iosFloatWrapper.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { MISC } from '../gral/constants';
+import { stopPropagation } from '../gral/helpers';
 import type { FloatPosition, FloatAlign } from '../components/floats';
 
 type Props = {
@@ -12,7 +13,9 @@ type Props = {
 };
 
 const IosFloatWrapper = (props: Props) => (
-  <div style={style.outer(props)}>{props.children}</div>
+  <div style={style.outer(props)} onScroll={stopPropagation}>
+    {props.children}
+  </div>
 );
 
 const style = {
@@ -21,6 +24,7 @@ const style = {
     [floatPosition === 'above' ? 'bottom' : 'top']: '100%',
     [floatAlign === 'right' ? 'right' : 'left']: 0,
     zIndex: floatZ != null ? floatZ : MISC.zMainFloatDelta,
+    maxHeight: 400,
   }),
 };
 
